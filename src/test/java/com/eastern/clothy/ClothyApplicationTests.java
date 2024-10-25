93c7f6fd82f15383d5e2dcaf0a8d4d82906b537f
Shehan Peiris <shehanmaleesha4884@gmail.com>
2024-10-25 11:55:07 +0530

    Clothy SpringBoot Backend

---
+++ src/test/java/com/eastern/clothy/ClothyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastern.clothy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
